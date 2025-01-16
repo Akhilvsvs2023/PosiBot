afda16ce5ed94e96895a69c70ffa4f40dd8ccc98
Akhil Vudatha <akhilvsvs1999@gmail.com>
2025-01-17 00:07:28 +0530

    initialCommit
    Base commit for project

---
+++ src/main/java/com/psx/PosibotApplication.java
@@ -0,0 +1,13 @@
+package com.psx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosibotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosibotApplication.class, args);
+	}
+
+}
