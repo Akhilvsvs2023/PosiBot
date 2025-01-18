a7d5c19c66ba0cb46ec70472418a5845d6a186b3
Akhil Vudatha <akhilvsvs1999@gmail.com>
2025-01-18 22:00:10 +0530

    Revised logic
    Removed hardcoded part and implemented dynamic generation of keywords based on DTO

--- src/main/java/com/psx/service/ClientService.java
+++ src/main/java/com/psx/service/ClientService.java
@@ -1,5 +1,6 @@
 package com.psx.service;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.sql.Date;
 import java.text.ParseException;
@@ -7,6 +8,7 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,10 +27,10 @@ public class ClientService implements ClientServiceI {
 	@Override
 	public String saveClientDetails(String clientInfo) throws Exception {
 		try {
-			Map<String, String> keyWordsMap = generateKeyWordMap();
-			Map<String, Class> dataTypesMap = generateDataTypeMap();
+			Map<String, Class> dataTypesMap = generateDataTypeMap(ClientDetails.class);
+			Map<String, String> keyWordsMap = generateKeyWordMap(dataTypesMap.keySet());
 			Map<String, Object> dataMap = extractClientDetails(keyWordsMap, dataTypesMap, clientInfo.toLowerCase());
-			ClientDetails data = prepareDTO(dataMap,dataTypesMap);
+			ClientDetails data = prepareDTO(dataMap, dataTypesMap);
 			repo.save(data);
 			return "Data saved successfully.";
 		} catch (Exception e) {
@@ -37,9 +39,9 @@ public class ClientService implements ClientServiceI {
 		}
 	}
 
-	private ClientDetails prepareDTO(Map<String, Object> dataMap,Map<String, Class> dataTypesMap) throws Exception{
+	private ClientDetails prepareDTO(Map<String, Object> dataMap, Map<String, Class> dataTypesMap) throws Exception {
 		ClientDetails dto = new ClientDetails();
-		for(Map.Entry<String, Object> entry : dataMap.entrySet()) {
+		for (Map.Entry<String, Object> entry : dataMap.entrySet()) {
 			String key = entry.getKey();
 			Object value = entry.getValue();
 			String methodName = "set" + key.substring(0, 1).toUpperCase() + key.substring(1);
@@ -49,18 +51,19 @@ public class ClientService implements ClientServiceI {
 		return dto;
 	}
 
-	private Map<String, Object> extractClientDetails(Map<String, String> keyWordsMap, Map<String, Class> dataTypesMap, String clientInfo) {
+	private Map<String, Object> extractClientDetails(Map<String, String> keyWordsMap, Map<String, Class> dataTypesMap,
+			String clientInfo) {
 		Map<String, Object> retValue = new LinkedHashMap<>();
 		Arrays.asList(clientInfo.split("\n")).forEach(x -> {
 			keyWordsMap.forEach((a, b) -> {
 				if (x.contains(a)) {
 					String value = x.substring(a.length());
 					if (dataTypesMap.get(b) == String.class) {
-						retValue.put(b, value);
+						retValue.put(b, value.trim());
 					} else if (dataTypesMap.get(b) == Date.class) {
-						try{
+						try {
 							retValue.put(b, extractDate(value));
-						}catch(Exception e) {
+						} catch (Exception e) {
 							throw new RuntimeException("Data is unclear, unable to parse date");
 						}
 					} else {
@@ -72,7 +75,7 @@ public class ClientService implements ClientServiceI {
 		return retValue;
 	}
 
-	private Date extractDate(String value) throws Exception{
+	private Date extractDate(String value) throws Exception {
 		SimpleDateFormat formatter = new SimpleDateFormat("dd MMMM yyyy");
 		try {
 			java.util.Date utilDate = formatter.parse(value.toLowerCase());
@@ -80,22 +83,23 @@ public class ClientService implements ClientServiceI {
 			return sqlDate;
 		} catch (ParseException e) {
 			throw e;
-		}		
+		}
 	}
 
-	private Map<String, Class> generateDataTypeMap() {
+	private Map<String, Class> generateDataTypeMap(Class<?> clazz) {
 		Map<String, Class> dataTypesMap = new LinkedHashMap<>();
-		dataTypesMap.put("name", String.class);
-		dataTypesMap.put("dob", Date.class);
+		for (Field field : clazz.getDeclaredFields()) {
+			dataTypesMap.put(field.getName(), field.getType());
+		}
 		return dataTypesMap;
 	}
 
-	private Map<String, String> generateKeyWordMap() {
+	private Map<String, String> generateKeyWordMap(Set<String> fields) {
 		Map<String, String> keyWordsMap = new LinkedHashMap<>();
-		keyWordsMap.put("my name is ", "name");
-		keyWordsMap.put("my dob is ", "dob");
+		for (String field : fields) {
+			keyWordsMap.put("my " + field + " is ", field);
+		}
 		return keyWordsMap;
 	}
-
 
 }
