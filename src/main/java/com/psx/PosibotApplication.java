25842aa1750e33899544b380ce2256cbdfb4ea1c
Akhil Vudatha <akhilvsvs1999@gmail.com>
2025-01-19 04:27:26 +0530

    Added Cleaner class and test cases
    Added CleanerUtils to clean data before inserting into dataBase and also added testcases to it

--- src/main/java/com/psx/PosibotApplication.java
+++ src/main/java/com/psx/PosibotApplication.java
@@ -1,13 +1,21 @@
 package com.psx;
 
+import java.util.regex.Pattern;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PosibotApplication {
 
+//	public static void main(String[] args) {
+//		SpringApplication.run(PosibotApplication.class, args);
+//	}
 	public static void main(String[] args) {
-		SpringApplication.run(PosibotApplication.class, args);
+		String a = "18abc1999";
+		System.out.println(Pattern.matches("^\\d{2}[A-Za-z]+\\d{4}$", a));
+		a = "abc";
+		System.out.println(Pattern.matches("^[A-Za-z]+$", a));
+		
 	}
-
 }
---
+++ src/main/java/com/psx/utils/CleanerUtils.java
@@ -0,0 +1,89 @@
+package com.psx.utils;
+
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+public class CleanerUtils {
+
+	public static Integer cleanInteger(String value) {
+		String result = value.replaceAll("\\s+", "").trim();
+		return Integer.parseInt(result);
+	}
+
+	public static Float cleanFloat(String value) {
+		String result = value.replaceAll("\\s+", "").trim();
+		return Float.parseFloat(result);
+	}
+
+	public static Double cleanDouble(String value) {
+		String result = value.replaceAll("\\s+", "").trim();
+		return Double.parseDouble(result);
+	}
+
+	public static Long cleanLong(String value) {
+		String result = value.replaceAll("\\s+", "").trim();
+		return Long.parseLong(result);
+	}
+
+	public static String cleanString(String value) {
+		String[] words = value.trim().split("\\s+");
+		StringBuilder result = new StringBuilder();
+		for (String word : words) {
+			if (word.length() > 1) {
+				result.append(Character.toUpperCase(word.charAt(0))).append(word.substring(1).toLowerCase())
+						.append(" ");
+			} else {
+				result.append(Character.toUpperCase(word.charAt(0))).append(" ");
+			}
+		}
+		return result.toString().trim();
+	}
+
+	public static Date cleanDate(String value) throws Exception {
+		value = value.replaceAll("\\s+", "").trim();
+		String[] arr = new String[3];
+		Map<Integer, String> map = generateMonthMap();
+		String result = "";
+		if (Pattern.matches("^(0[1-9]|[12][0-9]|3[01])(-|/)(0[1-9]|1[012])(-|/)((19|2[0-9])[0-9]{2})$", value)) {
+			arr = (value.contains("-")) ? value.split("-") : value.split("/");
+			result = arr[0] + " " + map.get(Integer.parseInt(arr[1])) + " " + arr[2];
+		} else if (Pattern.matches("^((19|2[0-9])[0-9]{2})(-|/)(0[1-9]|1[012])(-|/)(0[1-9]|[12][0-9]|3[01])$", value)) {
+			arr = (value.contains("-")) ? value.split("-") : value.split("/");
+			result = arr[2] + " " + map.get(Integer.parseInt(arr[1])) + " " + arr[0];
+		} else if (Pattern.matches("^\\d{2}[A-Za-z]+\\d{4}$", value)) {
+			result = value.substring(0, 2) + " " + value.substring(2, value.length() - 4) + " "	+ value.substring(value.length() - 4);
+		} else if (Pattern.matches("^\\d{4}[A-Za-z]+\\d{2}$", value)) {
+			result = value.substring(value.length() - 2) + " " + value.substring(4, value.length() - 2) + " " + value.substring(0, 4);
+		} else {
+			throw new Exception("Unregistered format :: "+value);
+		}
+		SimpleDateFormat formatter = new SimpleDateFormat("dd MMMM yyyy");
+		try {
+			java.util.Date utilDate = formatter.parse(result.toLowerCase());
+			return new Date(utilDate.getTime());
+		} catch (ParseException e) {
+			throw e;
+		}
+	}
+
+	public static Map<Integer, String> generateMonthMap() {
+		Map<Integer, String> monthMap = new LinkedHashMap<>();
+		monthMap.put(1, "January");
+		monthMap.put(2, "February");
+		monthMap.put(3, "March");
+		monthMap.put(4, "April");
+		monthMap.put(5, "May");
+		monthMap.put(6, "June");
+		monthMap.put(7, "July");
+		monthMap.put(8, "August");
+		monthMap.put(9, "September");
+		monthMap.put(10, "October");
+		monthMap.put(11, "November");
+		monthMap.put(12, "December");
+		return monthMap;
+	}
+}
